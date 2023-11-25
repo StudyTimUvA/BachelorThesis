4100ec21860468a333509e14950e6ea8e1ebdb18
Tim van der Hoeven <tim.van.der.hoeven@student.uva.nl>
2023-11-25 10:52:16 +0100

    Added file reading support, and reading of dlint file to a plot

--- scratches/main.py
+++ scratches/main.py
@@ -6,109 +6,170 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 from matplotlib.figure import Figure
 import tkinter as tk
 from dataclasses import dataclass
+import scapy as scapy
+from scapy.utils import PcapReader
+from scapy import layers
 
 @dataclass
 class PcapFile:
-    file_path: str
-    toggle_var: tk.BooleanVar
-    title: str
-    int_type: str
+	file_path: str
+	toggle_var: tk.BooleanVar
+	title: str
+	int_type: str
 
 class App:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("In-band Network Telemetry analysis")
-        self.root.geometry("1600x900")
+	def __init__(self, root):
+		self.root = root
+		self.root.title("In-band Network Telemetry analysis")
+		self.root.geometry("1600x900")
 
-        # Create a side menu
-        self.side_menu = tk.Frame(root, width=400, bg='grey', relief='raised', bd=2)
-        self.side_menu.pack(side='left', fill='both')
-        self.side_menu.update()
+		self.root.bind_all("q", self.key_pressed)
+		self.root.bind_all("esc", self.key_pressed)
 
-        demo1 = PcapFile("demo1.pcap", tk.BooleanVar(), "demo1", "DEMO")
-        demo2 = PcapFile("demo2.pcap", tk.BooleanVar(), "demo2", "DEMO")
-        self.pcap_files = [demo1, demo2]
+		# Create a side menu
+		self.side_menu = tk.Frame(root, width=400, bg='grey', relief='raised', bd=2)
+		self.side_menu.pack(side='left', fill='both')
+		self.side_menu.update()
 
-        self.file_path_selected = None
-        self.selected_int_type = None
+		demo1 = PcapFile("/home/tim/Desktop/AAUvA/Thesis/tcpdump_logs/dlint_1_flow/5mbps.pcap", tk.BooleanVar(), "dlint 5mbps", "dlint")
+		demo2 = PcapFile("/home/tim/Desktop/AAUvA/Thesis/tcpdump_logs/plint_1_flow/5mbps.pcap", tk.BooleanVar(), "plint 5mbps", "plint")
+		self.pcap_files = [demo1, demo2]
 
-        # Split side menu into three equal parts
-        self.side_menu_height = self.side_menu.winfo_height() / 3
-        self._create_top_side_menu()
-        self._create_middle_side_menu()
-        self._create_bottom_side_menu()
+		self.file_path_selected = None
+		self.selected_int_type = None
 
-    def _create_top_side_menu(self):
-        self.side_menu_top = tk.Frame(self.side_menu, width=400, height=self.side_menu_height, bg='grey', relief='raised', bd=2)
-        self.side_menu_top.pack(side='top', fill='both')
+		# Split side menu into three equal parts
+		self.side_menu_height = self.side_menu.winfo_height() / 3
+		self._create_top_side_menu()
+		self._create_middle_side_menu()
+		self._create_bottom_side_menu()
 
-        # Add a label to the top of the side menu
-        label = tk.Label(self.side_menu_top, text="Select a file to analyse", bg='grey')
-        label.pack(side='top', pady=10)
+		self._add_plot_to_view()
+		# x = np.linspace(0, 2, 100)
+		# self.add_to_plot(x, x, label="test")
 
-        # Add a file selector to the top of the side menu
-        self.file_selector_button = tk.Button(self.side_menu_top, text="Select file", command=self.select_file)
-        self.file_selector_button.pack(side='top', fill='both', padx=10, pady=10)
+	def key_pressed(self, event):
+		print("key pressed")
+		if event.char in ["q", "esc"]:
+			self.root.destroy()
 
-        # Add a drop down menu
-        dropdown_options = ["DINT", "PINT"]
-        self.selected_int_type = tk.StringVar(value="Select the type of INT")
-        drop_down_menu = tk.OptionMenu(self.side_menu_top, self.selected_int_type, *dropdown_options)
-        drop_down_menu.pack(side='top', fill='both', padx=10, pady=10)
+	def _create_top_side_menu(self):
+		self.side_menu_top = tk.Frame(self.side_menu, width=400, height=self.side_menu_height, bg='grey', relief='raised', bd=2)
+		self.side_menu_top.pack(side='top', fill='both')
 
-        # Create an add button
-        add_button = tk.Button(self.side_menu_top, text="Add", command=self.add_pcap_button)
-        add_button.pack(side='top', fill='both', padx=40, pady=10)
+		# Add a label to the top of the side menu
+		label = tk.Label(self.side_menu_top, text="Select a file to analyse", bg='grey')
+		label.pack(side='top', pady=10)
 
-    def select_file(self):
-        self.file_path_selected = tk.filedialog.askopenfilename()
-        self.file_selector_button.config(text=self.file_path_selected)
-        print(file_path)
+		# Add a file selector to the top of the side menu
+		self.file_selector_button = tk.Button(self.side_menu_top, text="Select file", command=self.select_file)
+		self.file_selector_button.pack(side='top', fill='both', padx=10, pady=10)
 
-    def add_pcap_button(self):
-        print("Add button pressed")
-        if self.file_path_selected is None:
-            print("No file selected")
-            return
-        if self.selected_int_type.get() == "Select the type of INT":
-            print("No INT type selected")
-            return
+		# Add a drop down menu
+		dropdown_options = ["DINT", "PINT"]
+		self.selected_int_type = tk.StringVar(value="Select the type of INT")
+		drop_down_menu = tk.OptionMenu(self.side_menu_top, self.selected_int_type, *dropdown_options)
+		drop_down_menu.pack(side='top', fill='both', padx=10, pady=10)
 
-        path = self.file_path_selected
-        title = path.split("/")[-1]
-        int_type = self.selected_int_type.get()
+		# Create an add button
+		add_button = tk.Button(self.side_menu_top, text="Add", command=self.add_pcap_button)
+		add_button.pack(side='top', fill='both', padx=40, pady=10)
 
-        self.pcap_files.append(PcapFile(path, tk.BooleanVar(), title, int_type))
-        self.redraw_middle_menu_side()
+	def select_file(self):
+		self.file_path_selected = tk.filedialog.askopenfilename()
+		self.file_selector_button.config(text=self.file_path_selected)
+		print(file_path)
 
-    def _create_middle_side_menu(self):
-        self.side_menu_middle = tk.Frame(self.side_menu, width=400, height=self.side_menu_height, bg='grey', relief='raised', bd=2)
-        self.side_menu_middle.pack(side='top', fill='both')
+	def add_pcap_button(self):
+		print("Add button pressed")
+		if self.file_path_selected is None:
+			print("No file selected")
+			return
+		if self.selected_int_type.get() == "Select the type of INT":
+			print("No INT type selected")
+			return
 
-        # Add a label to the top of the side menu
-        label = tk.Label(self.side_menu_middle, text="toggle captures", bg='grey')
-        label.pack(side='top', pady=10)
+		path = self.file_path_selected
+		title = path.split("/")[-1]
+		int_type = self.selected_int_type.get()
 
-        # Create toggle buttons for each line
-        for line in self.pcap_files:
-            toggle_var = tk.BooleanVar()
-            toggle_button = tk.Checkbutton(self.side_menu_middle, text=line.title, variable=line.toggle_var, onvalue=True, offvalue=False, bg='grey', command=self.toggle_button)
-            toggle_button.pack(anchor='w')
+		self.pcap_files.append(PcapFile(path, tk.BooleanVar(), title, int_type))
+		self.redraw_middle_menu_side()
 
-    def redraw_middle_menu_side(self):
-        self.side_menu_middle.destroy()
-        self.side_menu_bottom.destroy()
-        self._create_middle_side_menu()
-        self._create_bottom_side_menu()
+	def _create_middle_side_menu(self):
+		self.side_menu_middle = tk.Frame(self.side_menu, width=400, height=self.side_menu_height, bg='grey', relief='raised', bd=2)
+		self.side_menu_middle.pack(side='top', fill='both')
 
-    def toggle_button(self):
-        print("toggle button pressed")
-        for file in self.pcap_files:
-            print(file.toggle_var.get())
+		# Add a label to the top of the side menu
+		label = tk.Label(self.side_menu_middle, text="toggle captures", bg='grey')
+		label.pack(side='top', pady=10)
 
-    def _create_bottom_side_menu(self):
-        self.side_menu_bottom = tk.Frame(self.side_menu, width=400, height=self.side_menu_height, bg='grey', relief='raised', bd=2)
-        self.side_menu_bottom.pack(side='top', fill='both')
+		# Create toggle buttons for each line
+		for line in self.pcap_files:
+			toggle_var = tk.BooleanVar()
+			toggle_button = tk.Checkbutton(self.side_menu_middle, text=line.title, variable=line.toggle_var, onvalue=True, offvalue=False, bg='grey', command=self.toggle_button)
+			toggle_button.pack(anchor='w')
+
+	def redraw_middle_menu_side(self):
+		self.side_menu_middle.destroy()
+		self.side_menu_bottom.destroy()
+		self._create_middle_side_menu()
+		self._create_bottom_side_menu()
+
+	def toggle_button(self):
+		print("toggle button pressed")
+
+		# Clear the plot
+		self.plot.clear()
+
+		for file in self.pcap_files:
+			if file.toggle_var.get():
+				# Draw the file
+				delays = self.get_delays_from_pcap(file.file_path)
+				x = np.linspace(0, len(delays), len(delays))
+				self.add_to_plot(x, delays, label=file.title)
+
+	def get_delays_from_pcap(self, pcap_file):
+		delays = []
+		for packet in PcapReader(pcap_file):
+			for option in packet.getlayer(layers.inet.TCP).options:
+				if option[0] == 114:  # hex 0x72
+					values = ''.join([hex(x)[2:].zfill(2) for x in option[1]])
+
+					kind = option[0]
+					switch_id = int(values[0:4], 16)
+					delay = int(values[4:20], 16)
+
+					if switch_id == 0:
+						continue
+
+					delays.append(delay)
+
+		return delays
+
+	def _create_bottom_side_menu(self):
+		self.side_menu_bottom = tk.Frame(self.side_menu, width=400, height=self.side_menu_height, bg='grey', relief='raised', bd=2)
+		self.side_menu_bottom.pack(side='top', fill='both')
+
+	def _add_plot_to_view(self):
+		# Create a figure
+		self.figure = Figure(figsize=(5, 5), dpi=100)
+		self.plot = self.figure.add_subplot(111)
+
+		# Create a canvas
+		self.canvas = FigureCanvasTkAgg(self.figure, self.root)
+		self.canvas.draw()
+		self.canvas.get_tk_widget().pack(side='top', fill='both', expand=True)
+
+		# Create a toolbar
+		self.toolbar = NavigationToolbar2Tk(self.canvas, self.root)
+		self.toolbar.update()
+		self.canvas.get_tk_widget().pack(side='top', fill='both', expand=True)
+
+	def add_to_plot(self, x, y, label=None):
+		self.plot.plot(x, y, label=label)
+		self.plot.legend()
+		self.canvas.draw()
 
 
 
